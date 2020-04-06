f20560db95b9c8348df4ae4a487ef347e1756410
jstehli <jstehli@student.ethz.ch>
2020-04-06 16:52:08 +0200

    add plugin manager for vim

--- install.sh
+++ install.sh
@@ -6,3 +6,8 @@
 
 # vimrc
 ln -sv ~/.dotfiles/.vimrc ~
+
+# plugin manager: vimplug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
