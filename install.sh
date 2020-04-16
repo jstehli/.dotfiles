c4cb6414c9c65bf3bc918ae2064e009cdd78b90c
jstehli <jstehli@student.ethz.ch>
2020-04-16 22:01:54 +0200

    removed vim plugin manager

--- install.sh
+++ install.sh
@@ -13,14 +13,9 @@ rm ~/.bash_aliases
 ln -sv ~/.dotfiles/.bash_aliases ~
 
 
-
 # *** vim ***
 
-# vimrc
 rm ~/.vimrc
-ln -sv ~/.dotfiles/.vimrc ~
-
-# plugin manager: vimplug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+rm -rf ~/.vim
+ln -sv ~/.dotfiles/.vim ~
 
