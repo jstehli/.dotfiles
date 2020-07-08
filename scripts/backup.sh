eb7097a135591879768b2e603581562159b90d39
Jonas Stehli <jonas.stehli@jsounds.ch>
2020-07-08 15:15:15 +0200

    backup script finished

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,53 +1,90 @@
+#!/bin/bash
+
 # backup script that generates daily / weekly / monthly backups
 # this checks if the backup location is available, i.e., you can 
 # use this with an external disk.
 # put this in /etc/cron.hourly
 # author: Jonas Stehli
 
-# set some global variables
-BACKUP_MOUNTPOINT="/run/media/jonas/js-backup"
-CURRENTDATE=`date +"%Y-%m-%d %T"`
-X_USER="jonas"
-X_USER_ID="1000"
 
-# notify function
-notify () {
-    sudo -u $X_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$X_USER_ID/bus notify-send "${2:-BACKUP}" "$1"
+# set some global variables - edit the mountpoint according to your needs
+BACKUP_MOUNTPOINT="/run/media/jonas/js-backup"
+NOW=`date +"%s"`
+DATE_STR=`date "+%Y-%m-%d %H:%M:%S"`
+LOGDIR=$BACKUP_MOUNTPOINT"/logs"
+TIMES=$LOGDIR"/times.txt"
+LOGFILE=$LOGDIR"/log.txt"
+
+# helper functions
+update_times() {
+    rm -f $TIMES
+    touch $TIMES
+    local time_array=($@)
+    for time in "${time_array[@]}"; do
+        echo $time >> $TIMES
+    done
 }
 
+backup_command() {
+    local target_dir=$1
+    rsync -aAXHq --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/.snapshots/*","/home/jonas/Dropbox/*"} / $target_dir
+}
 
-# check that disk is mounted
+# check that disk is mounted and make logdir / logfile
 if [ ! -d "$BACKUP_MOUNTPOINT" ]; then
-    notify "Backup [$frequency] failed: destination not found."
     exit 1
 fi
+mkdir -p $LOGDIR
+touch $LOGFILE
 
-frequency=$1
-target_directory=$BACKUP_MOUNTPOINT"/"$frequency
-logfile=$target_directory"/log_backup_"$frequency".txt"
-backup_directory=$target_directory/"backup_data"
-
-if [ ! -d "$target_directory" ]; then
-    mkdir $target_directory
+# read the last times backup was done
+do_daily=false
+do_weekly=false
+do_monthly=false
+if [ -f $TIMES ]; then
+    readarray -t backup_times < $TIMES
+    if [[ $(expr $NOW - ${backup_times[0]}) -gt 86400 ]]; then
+        do_daily=true
+    fi
+    if [[ $(expr $NOW - ${backup_times[1]}) -gt 604800 ]]; then
+        do_weekly=true
+    fi
+    if [[ $(expr $NOW - ${backup_times[2]}) -gt 5292000 ]]; then
+        do_monthly=true
+    fi
+else
+    backup_times=(0 0 0)
+    do_daily=true
+    do_weekly=true
+    do_monthly=true
 fi
-if [ ! -f $logfile ]; then
-    touch $logfile
+
+if $do_daily; then
+    backup_directory=$BACKUP_MOUNTPOINT"/daily"
+    mkdir -p $backup_directory
+    backup_command $backup_directory
+    backup_times[0]=$NOW
+    update_times "${backup_times[@]}"
+    echo "Daily backup done: $DATE_STR" >> $LOGFILE
 fi
-if [ ! -d $backup_directory ]; then
-    mkdir $backup_directory
+
+if $do_weekly; then
+    backup_directory=$BACKUP_MOUNTPOINT"/weekly"
+    mkdir -p $backup_directory
+    backup_command $backup_directory
+    backup_times[1]=$NOW
+    update_times "${backup_times[@]}"
+    echo "Weekly backup done: $DATE_STR" >> $LOGFILE
 fi
 
-notify "$(echo -e "Starting full system backup [$frequency] to: $backup_directory")"
-
-logstr=$CURRENTDATE"   Starting "$frequency" backup"
-echo $logstr >> $logfile
-
-
-sudo rsync -aAXHv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/.snapshots/*"} / $backup_directory
-
-logstr=$CURRENTDATE"   "$frequency" backup finished."
-echo $logstr >> $logfile
-notify "Full system backup done [$frequency]."
+if $do_monthly; then
+    backup_directory=$BACKUP_MOUNTPOINT"/monthly"
+    mkdir -p $backup_directory
+    backup_command $backup_directory
+    backup_times[2]=$NOW
+    update_times "${backup_times[@]}"
+    echo "Monthly backup done: $DATE_STR" >> $LOGFILE
+fi
 
 
 exit 0
